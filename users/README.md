d5466fdb58a2a3a9ab02bd906deafb77693a706d
nalek0 <belash.aleksandr777@gmail.com>
2025-02-02 18:57:23 +0300

    Initial commit

---
+++ users/README.md
@@ -0,0 +1,60 @@
+# User management
+
+This folder provides several script to manage users for the different supported VPN interfaces
+
+## Installation
+
+### Prerequisites
+
+* Python 3
+* Pip (python package installer)
+
+### Installation steps
+
+#### 1. Clone repository
+```bash
+git clone https://github.com/DobbyVPN/DobbyVPN-server.git
+cd DobbyVPN-server
+```
+
+#### 2. Install required python libraries
+
+```bash
+pip install users/requirements.txt
+```
+
+## Run
+
+For each supported vpn interface there is separate `.py` script, that should be run at the root repository directory.
+Scripts have the same call arguments format:
+
+```bash
+python3 users/<vpn interface>_management.py ARGS
+```
+
+`ARGS` defines command and required parameters
+
+### Supported commands
+
+#### Add key command
+
+Add keys to the VPN to the provided user.
+
+> ARGS=add <user name>
+
+#### Delete key command
+
+Remove keys from the VPN for the provided user.
+
+> ARGS=del <user name>
+
+#### List keys
+
+Print all user keys if the user is given, othervise prints all keys
+
+> ARGS=list <optional user name>
+
+## Supported VPNs
+
+* Outline VPN
+* AmneziaWG
---
+++ users/outline_management.py
@@ -0,0 +1,72 @@
+import sys
+
+def print_help():
+	program_name = sys.argv[0]
+	help_message = f"""python3 {program_name} help
+	prints script informations
+python3 {program_name} add <user_name>
+	add new user to the Outline and return key and cloak client config
+python3 {program_name} list [user_name]
+	print list of all user keys or list of all possible keys if no user name provided
+python3 {program_name} del <user_name>
+	remove user keys"""
+
+	print(help_message)
+
+def help_command(arguments):
+	print_help()
+
+	if len(arguments) > 0:
+		exit(1)
+	else:
+		exit(0)
+
+def add_command(arguments):
+	if len(arguments) > 1 or len(arguments) == 0:
+		print_help()
+		exit(1)
+	else:
+		user_name = arguments[0]
+
+		# TODO
+
+def del_command(arguments):
+	if len(arguments) > 1 or len(arguments) == 0:
+		print_help()
+		exit(1)
+	else:
+		user_name = arguments[0]
+
+		# TODO
+
+def list_command(arguments):
+	if len(arguments) == 1:
+		user_name = arguments[0]
+
+		# TODO
+	elif len(arguments) == 0:
+		# TODO
+		pass
+	else:
+		print_help()
+		exit(1)
+
+SUPPORTED_COMMANDS = {
+	"help": help_command,
+	"add": add_command,
+	"del": del_command,
+	"list": list_command,
+}
+
+if __name__ == "__main__":
+	if len(sys.argv) <= 1:
+		print_help()
+		exit(1)
+
+	command_name = sys.argv[1]
+
+	if command_name not in SUPPORTED_COMMANDS.keys():
+		print_help()
+		exit(1)
+
+	SUPPORTED_COMMANDS[command_name](sys.argv[2:])
