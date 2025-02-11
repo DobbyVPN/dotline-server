f011c48a6b9c5195da81c1d75f479ddf3f6975e5
nalek0 <belash.aleksandr777@gmail.com>
2025-02-11 20:33:26 +0300

    automatic awg server reload

--- awg/start.sh
+++ awg/start.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+config_hash=$(sha1sum wg0.conf)
 awg-quick up wg0
-while true; do sleep 1; done
+echo "[$(date)] awg server initialisation" >> awg.log
+
+while true; do
+	new_config_hash=$(sha1sum wg0.conf)
+
+	if [ "$config_hash" != "$new_config_hash" ]; then
+		config_hash=$new_config_hash
+		awg-quick down wg0
+		awg-quick up wg0
+		echo "[$(date)] awg server reload" >> awg.log
+	fi
+
+	sleep 5
+done
 
 tail -f /dev/null
