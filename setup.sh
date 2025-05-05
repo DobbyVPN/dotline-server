3069f11c2bea120a623752bc192c72ddc8b5502a
Alex P. <alex@nocloud.today>
2025-05-05 22:25:54 +0200

    minor setup changes

--- setup.sh
+++ setup.sh
@@ -10,11 +10,10 @@ apt install -y gettext jq
 
 current_cc=$(sysctl -n net.ipv4.tcp_congestion_control)
 
-# Enable BBR, if not in place
+# Enable BBR, if it is not in place
 if [ "$current_cc" != "bbr" ]; then
-    cat << EOF >> /etc/sysctl.d/10-custom-kernel-bbr.conf
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
+    echo 'net.core.default_qdisc=fq' > /etc/sysctl.d/10-custom-kernel-bbr.conf
+    echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.d/10-custom-kernel-bbr.conf
 EOF
     service procps force-reload
 fi
@@ -24,11 +23,11 @@ if ! command -v docker &> /dev/null; then
     wget -O - https://get.docker.com | sudo bash 
 fi
 
-
+# Get user defined variables
 read -p "Enter 'face' domain name: " DOMAIN_NAME
 read -p "Enter Outline's IP & port (as 'IP:port'): " OUTLINE_IP_PORT
 
-touch .env
+touch .env # to avoid getting it created as a folder
 
 # Configuring Cloak server
 docker run --rm -v $(pwd)/.env:/.env ghcr.io/dobbyvpn/dobbyvpn-server/cloak-server:v2 sh -c \
